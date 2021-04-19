ccf3698f26ddbb205440adfc2ffc768acf687c1a
Mayukh Sasmal <mayukhsasmal16@gmail.com>
2021-04-19 21:39:01 +0530

    added-list-access-from-json-data

--- Project 10 - A Simple React App/react-app/src/components/App.js
+++ Project 10 - A Simple React App/react-app/src/components/App.js
@@ -1,24 +1,56 @@
+import React,{Component} from 'react';
 import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 bg-white">
-              <div className="container">
-                <AddAppointments/>
-                <SearchAppointments/>
-                <ListAppointments/>
+
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      myApponitments: [],
+      lastIndex: 0
+    };
+  }
+
+  componentDidMount(){
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({lastIndex: this.state.lastIndex +1});
+          return item;
+        })
+
+      this.setState({
+        myApponitments: apts
+      });
+
+      });
+      
+  }
+
+  render(){
+
+    return (
+      <main className="page bg-white" id="petratings">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 bg-white">
+                <div className="container">
+                  <AddAppointments/>
+                  <SearchAppointments/>
+                  <ListAppointments appointments={this.state.myApponitments}/>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
-  );
+        </main>
+    );
+  }
 }
 
 export default App;
--- Project 10 - A Simple React App/react-app/src/components/ListAppointments.js
+++ Project 10 - A Simple React App/react-app/src/components/ListAppointments.js
@@ -2,9 +2,31 @@ import React, {Component} from 'react';
 
 class ListAppointments extends Component {
     render(){
+        
         return(
-            <div>
-                List of Appointments
+            
+            <div className="appointment-list item-list mb-3">
+                {this.props.appointments.map(item => (
+                    <div className="pet-item col media py-3" key={item.aptId}>
+                        <div className="mr-3">
+                            <button className="pet-delete btn btn-sm btn-danger">X</button>
+                        </div>
+
+                        <div className="pet-info media-body">
+                            <div className="pet-head d-flex">
+                                <span className="pet-name">{item.aptId+1}<>&nbsp;</>{item.petName}</span>
+                                
+                                <span className="apt-date ml-auto">{item.aptDate}</span>
+                            </div>
+
+                            <div className="owner-name">
+                                <span className="label-item">Owner: </span>
+                                <span>{item.ownerName}</span>
+                            </div>
+                            <div className="apt-notes">{item.aptNotes}</div>
+                        </div>
+                    </div>
+                ))}
             </div>
         );
     }
