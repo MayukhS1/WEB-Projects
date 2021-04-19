cf67705cf7c3667eafcd48f1943cb35ef5df6b07
Mayukh Sasmal <mayukhsasmal16@gmail.com>
2021-04-19 20:08:09 +0530

    components added to project 10

---
+++ Project 10 - A Simple React App/react-app/src/components/AddAppointments.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AddAppointments extends Component {
+    render(){
+        return(
+            <div>
+                Add Appointments
+            </div>
+        );
+    }
+}
+
+export default AddAppointments;
---
+++ Project 10 - A Simple React App/react-app/src/components/SearchAppointments.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SearchAppointments extends Component {
+    render(){
+        return(
+            <div>
+                Search for Appointments
+            </div>
+        );
+    }
+}
+
+export default SearchAppointments;
