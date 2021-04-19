a195ff7ff1e94a0bf5cce0ad7279a686efcc6b9c
Mayukh Sasmal <mayukhsasmal16@gmail.com>
2021-04-19 21:51:57 +0530

    json-data-manupulated

--- Project 10 - A Simple React App/react-app/src/components/ListAppointments.js
+++ Project 10 - A Simple React App/react-app/src/components/ListAppointments.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { FaTimes } from 'react-icons/fa';
+import Moment from 'react-moment';
 
 class ListAppointments extends Component {
     render(){
@@ -9,14 +11,24 @@ class ListAppointments extends Component {
                 {this.props.appointments.map(item => (
                     <div className="pet-item col media py-3" key={item.aptId}>
                         <div className="mr-3">
-                            <button className="pet-delete btn btn-sm btn-danger">X</button>
+                            <button className="pet-delete btn btn-sm btn-danger">
+                                <FaTimes/>
+                            </button>
                         </div>
 
                         <div className="pet-info media-body">
                             <div className="pet-head d-flex">
-                                <span className="pet-name">{item.aptId+1}<>&nbsp;</>{item.petName}</span>
+                                <span className="pet-name">
+                                    {item.aptId+1}<>&nbsp;</>{item.petName}
+                                </span>
                                 
-                                <span className="apt-date ml-auto">{item.aptDate}</span>
+                                <span className="apt-date ml-auto">
+                                    <Moment 
+                                        date={item.aptDate}
+                                        parse="YYYY-MM-DD hh:mm"
+                                        format="MMMM Do YYYY h:ma"
+                                    />
+                                </span>
                             </div>
 
                             <div className="owner-name">
