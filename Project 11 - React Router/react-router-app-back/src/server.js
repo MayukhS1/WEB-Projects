11db0163afe95c7ac1d2b568dcb8fbf1e7199f4a
Mayukh Sasmal <mayukhsasmal16@gmail.com>
2021-05-07 20:10:46 +0530

    mongodb-connected

--- Project 11 - React Router/react-router-app-back/src/server.js
+++ Project 11 - React Router/react-router-app-back/src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {MongoClient} from 'mongodb';
+
 
 // fake database for now
 
@@ -27,12 +29,63 @@ app.use(bodyParser.json());
 // app.get('/hello/:name', (req,res) => res.send(`Hello ${req.params.name}`));
 // app.post('/hello', (req, res)=> res.send(`Hello Again, ${req.body.name}!`));
 
-app.post ('/api/articles/:name/upvote', (req, res) =>{
+const withDB = async (operations, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
+    
+        await operations(db);
+    
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+}
+
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017', //27017 is default for mongoDB
+        {useNewUrlParser: true});  
+
+        const db = client.db('my-blog');
+
+        const articleInfo = await db.collection('articles').findOne({name: articleName});
+
+        res.status(200).json(articleInfo);
+
+        client.close(); //close connection with the DB
+    } catch (error) {
+        res.status(500).json({message: 'Something went wrong!', error});
+    }
+    
+
+});
+
+app.post ('/api/articles/:name/upvote', async (req, res) =>{
+    
     const articleName = req.params.name;
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', //27017 is default for mongoDB
+        {useUnifiedTopology: true, useNewUrlParser: true}); 
 
-    articlesInfo[articleName].upvotes += 1;
+        const db = client.db('my-blog');
 
-    res.status(200).send(`${articleName} has : ${articlesInfo[articleName].upvotes} upvotes`);
+        const articleInfo = await db.collection('articles').findOne({name: articleName});
+
+        await db.collection('articles').updateOne({name: articleName},{
+            '$set': { upvotes: articleInfo.upvotes+1,}
+        });
+
+        const updatedArticleInfo = await db.collection('articles').findOne({name: articleName});
+
+        res.status(200).json(updatedArticleInfo);
+        client.close();
+
+    } catch (error) {
+        res.status(500).json({message: 'Something went wrong!', error});
+    }
 } );
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
