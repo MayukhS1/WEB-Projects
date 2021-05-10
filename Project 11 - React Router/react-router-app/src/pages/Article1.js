1e07c3203857972a9e704b77e1f0db505cc6b34e
Mayukh Sasmal <mayukhsasmal16@gmail.com>
2021-05-10 21:57:02 +0530

    upvote-button-added

--- Project 11 - React Router/react-router-app/src/pages/Article1.js
+++ Project 11 - React Router/react-router-app/src/pages/Article1.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import ArticleList from "../components/ArticleList";
 import articles from "./Article-content";
 import NotFoundPage from "./NotFoundPage";
-
+import CommentList from "../components/commentList";
+import UpvoteSection from "../components/upvoteSecion";
 const ArticlePage = ( {match} ) => {
+
+    // fetch('/api/articles/...',{
+    //     method: 'POST',
+    //     body: 
+    // })
     const name = match.params.name;
     const article = articles.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({upvotes:0, comments: []});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            // console.log(result);
+            const body = await result.json();
+            // console.log(body);
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]); //use effec will be called when even name changes
 
     if(!article){
         return(
@@ -20,9 +39,11 @@ const ArticlePage = ( {match} ) => {
     return (
         <>
             <h1>{article.title}</h1>
+            <UpvoteSection articleName={name} setArticleInfo={setArticleInfo} upvotes={articleInfo.upvotes}/>
             {article.content.map((paragraph, key ) => (
                 <p key={key}>{paragraph}</p>
             ))}
+            <CommentList comments={articleInfo.comments}/>
             <h3>Other Articles:</h3>
             <ArticleList articles={otherArticles}/>
         </>
